688f4a4423ad749ef3a7b1a2033a310f68c5a39d
KennedyHG <kennedy.hgarcia@senacsp.edu.br>
2024-03-14 17:32:35 -0300

    exercicio JS03 - 14/03

---
+++ js/acoes.js
@@ -0,0 +1,13 @@
+//alert('funcionou?');
+'user strict'
+
+
+//document.getElementById('caixa').innerHTML = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
+
+<a >Azul</a>
+
+
+
+<button
+        onclick="document.getElementById('texto').innerHTML = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s'">Clique
+        aqui</button>
