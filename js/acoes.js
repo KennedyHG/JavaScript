975835d8ab969b99bab3a03c100ae1511c7d6fe4
KennedyHG <kennedy.hgarcia@senacsp.edu.br>
2024-03-15 17:29:30 -0300

    exercicio e JS

--- js/acoes.js
+++ js/acoes.js
@@ -4,10 +4,16 @@
 
 //document.getElementById('caixa').innerHTML = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
 
-<a >Azul</a>
+var botaoazul 
+var botaoverde
+var botaolaranja
 
 
 
-<button
-        onclick="document.getElementById('texto').innerHTML = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s'">Clique
-        aqui</button>
+<h3 id="frase"></h3>
+
+
+
+id="azul", onclick="document.getElementById('frase').innerHTML = 'Azul significa Harmonia e Maturidade', document.body.style.backgroundColor = 'blue'">Azul;
+
+
