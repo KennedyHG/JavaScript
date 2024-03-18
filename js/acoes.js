3446d858679bbf0a6796d04563a92b243d295333
KennedyHG <kennedy.hgarcia@senacsp.edu.br>
2024-03-18 17:36:00 -0300

    aula 18/03

--- js/acoes.js
+++ js/acoes.js
@@ -4,16 +4,20 @@
 
 //document.getElementById('caixa').innerHTML = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s'
 
-var botaoazul 
-var botaoverde
-var botaolaranja
+var btblue = document.getElementById ('azul');
+var btgreen = document.getElementById ('verde');
+var btorange = document.getElementById ('laranja');
+var textoh3 = document.querySelector ('textoone');
+
+
+btblue.onmouseover = () => document.body.style.backgroundColor = "blue";
+btblue.onclick = () => textoh3.textContent = 'azul significa...';
+btgreen.onmouseover = () => document.body.style.backgroundColor = "green";
+btgreen.onclick = () => textoh3.textContent = 'verde significa...';
+btorange.onmouseover = () => document.body.style.backgroundColor = "orange";
+btorange.onclick = () => textoh3.textContent = 'laranja significa...';
 
 
 
-<h3 id="frase"></h3>
-
-
-
-id="azul", onclick="document.getElementById('frase').innerHTML = 'Azul significa Harmonia e Maturidade', document.body.style.backgroundColor = 'blue'">Azul;
 
 
