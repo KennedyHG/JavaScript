a9c7b9e73b982f668921cbbf01b1ad3d173b6801
KennedyHG <kennedy.hgarcia@senacsp.edu.br>
2024-04-02 17:19:28 -0300

    Aula 02/04/2024

---
+++ projeto Utopia/js/acoes.js
@@ -0,0 +1,37 @@
+//alert('funcionou')
+var btnmostrar = document.getElementById('iconMenu'); // icone  do menu
+var menu = document.getElementById('itens'); // menu a ser ocultado e/ou mostrado
+var logo = document.getElementById('logotipo'); // logotipo do site
+var btnOcultar = document.getElementById('iconClose') // icone fechar do menu
+
+//Ações do menu
+
+btnmostrar.addEventListener('click', function() {
+    if(menu.style.display === 'block')
+    {
+        menu.style.display = 'none';
+    }
+     else{
+        menu.style.display = 'block';
+    }
+
+})
+
+btnmostrar.addEventListener('click', function() {
+    if(logo.style.display === 'none')
+    {
+        logo.style.display = 'block';
+    }
+     else{
+        logo.style.display = 'none';
+    
+    }
+
+})
+
+btnOcultar.addEventListener('click', function() {
+ menu.style.display = 'none';
+ logo.style.display = 'block'
+})
+
+
